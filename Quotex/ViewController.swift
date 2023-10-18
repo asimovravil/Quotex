fc7b93b77322353dd48948740e3ec3db902d5f2c
Ravil <star.toni@mail.ru>
2023-10-18 22:02:41 +0600

    Initial Commit

---
+++ Quotex/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Quotex
+//
+//  Created by Ravil on 18.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
